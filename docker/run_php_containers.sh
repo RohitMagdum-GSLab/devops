9028c04ebc8cce9ba3aeb6ad538a18d72433411f
Pavan Gupta <pvn.gupta.2009@gmail.com>
2016-03-02 12:46:14 +0530

    updated php runner

--- docker/run_php_containers.sh
+++ docker/run_php_containers.sh
@@ -13,8 +13,17 @@ docker run -itdp 1454:22 u14_php5445
 docker run -itdp 1455:22 u14_php5530
 docker run -itdp 1456:22 u14_php5617
 
-# Centos 7
-docker run -itdp 7553:22 c7_php5530
+# ============================      CENTOS Containers    =============================
 
 # Centos 6 
+docker run -itdp 6532:22 c6_php5329
+docker run -itdp 6544:22 c6_php5445
 docker run -itdp 6553:22 c6_php5530
+docker run -itdp 6561:22 c6_php5617
+
+# Centos 7
+docker run -itdp 7532:22 c7_php5329
+docker run -itdp 7544:22 c7_php5445
+docker run -itdp 7553:22 c7_php5530
+docker run -itdp 7561:22 c7_php5617
+
