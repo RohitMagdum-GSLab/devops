b6a1a3a784529aefabfe9c701e5b7f51d6d47106
Pavan Gupta <pvn.gupta.2009@gmail.com>
2016-03-02 12:26:04 +0530

    update py container runner

--- docker/run_py_containers.sh
+++ docker/run_py_containers.sh
@@ -1,11 +1,11 @@
 # run.sh will launch python docker containers in one go
 
 # Ubuntu 12 
-docker run -itdp 12273:22 u12_py273
+docker run -itdp 1227:22 u12_py273
 
 # Ubuntu 14
-docker run -itdp 14269:22 u14_py269
-docker run -itdp 14276:22 u14_py276
+docker run -itdp 1426:22 u14_py269
+docker run -itdp 1427:22 u14_py276
 docker run -itdp 1434:22 u14_py34
 docker run -itdp 1435:22 u14_py35
 
@@ -18,10 +18,3 @@ docker run -itdp 7269:22 c7_py269
 docker run -itdp 7275:22 c7_py275
 docker run -itdp 7340:22 c7_py34
 docker run -itdp 7350:22 c7_py35
-
-
-
-
-
-
-
